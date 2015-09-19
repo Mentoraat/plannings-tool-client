ccf7e95e1023968074c4780738437a6e2c935ca9
Tim van der Lippe <tvanderlippe@gmail.com>
2015-09-19 21:46:43 +0200

    Clean up of useless files. Fix some images sources.

--- app/scripts/services/eventService.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('planningtoolApp')
-    .service('eventService', function($log, httpService) {
-
-    this.get = function(user) {
-      return httpService.get('users/USER-' + user + '/occurrences');
-    };
-});
